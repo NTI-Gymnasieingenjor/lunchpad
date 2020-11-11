aba08000cf980bcad7b2648f0b285794d3360de2
Gustav.Sahlberg <gustav.sahlberg2001@gmail.com>
2020-11-11 11:49:36 +0100

    Refactored tests
    Co-Authored-By: Emil Andersen <54682521+piloteer44@users.noreply.github.com>

--- tests/test_blipp_cases.py
+++ tests/test_blipp_cases.py
@@ -1,35 +1,39 @@
 #!/usr/bin/env python3
-import subprocess, time, os, signal, psutil, sys
+import subprocess, time, psutil, sys
 from pynput.keyboard import Key, Controller
 
-args = ["python","lunchpad.py"]
-p = subprocess.Popen(args, stdout=subprocess.PIPE, stdin=subprocess.PIPE, shell=True)
 
-tests = [
-    ["12348910", "Okänd nyckeltagg"],
-    ["536956614", "Nekat"],
-    ["101129785", "Godkänt"],
-    ["101129785", "Dubbel skann"]
-]
+def correct_text():
+    time.sleep(2)
+    keyboard = Controller()
 
-time.sleep(2)
-keyboard = Controller()
+    for test in tests:
+        keyboard.type(test[0])
+        keyboard.press(Key.enter)
+        keyboard.release(Key.enter)
+        time.sleep(1)
+        
+    keyboard.press(Key.esc)
+    keyboard.release(Key.esc)
 
-for test in tests:
-    keyboard.type(test[0])
-    keyboard.press(Key.enter)
-    keyboard.release(Key.enter)
-    time.sleep(1)
+    for test in tests:
+        res = p.stdout.readline().decode("latin-1").strip()
+        if res == test[1]:
+            print("\u001b[32mTest successful\u001b[0m")
+        else:
+            print("\u001b[31mTest failed\u001b[0m")
+            sys.exit(1)
 
 
-keyboard.press(Key.esc)
-keyboard.release(Key.esc)
+if __name__ == '__main__':
+    args = ["python","lunchpad.py"]
+    p = subprocess.Popen(args, stdout=subprocess.PIPE, stdin=subprocess.PIPE, shell=True)
 
-for test in tests:
-    res = p.stdout.readline().decode("latin-1").strip()
-    if res == test[1]:
-        print("\u001b[32mTest successful\u001b[0m")
-    else:
-        print("\u001b[31mTest failed\u001b[0m")
-        sys.exit(1)
+    tests = [
+        ["12348910", "Okänd nyckeltagg"],
+        ["536956614", "Nekat"],
+        ["101129785", "Godkänt"],
+        ["101129785", "Dubbel skann"]
+    ]
+    correct_text()
 
