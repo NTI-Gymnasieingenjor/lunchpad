2198d330a05be4a4de4e89f8953aacca3a690358
magnuslif <70268658+magnuslif@users.noreply.github.com>
2020-11-12 09:31:08 +0100

    Added the "improved text" changes to the updated code

--- lunchpad.py
+++ lunchpad.py
@@ -69,7 +69,7 @@ def blipp_your_tagg():
     def _timeout():
         global timer
         turtle.clear()
-        turtle.write("VÄNLIGEN SKANNA DIN NYCKELTAGG NEDAN", font=style, align='center')
+        turtle.write("VÄNLIGEN SKANNA DIN NYCKELTAGG TILL VÄNSTER", font=style, align='center')
         turtle.bgcolor("black")
         timer = None
 
@@ -115,7 +115,7 @@ def handle_enter(window, style):
     times_match = find_matching_lunch_time(tag_match[0])
     if(not (len(times_match) > 0)):
         print("Ingen matchande lunchtid")
-        write_text_turtle(window, turtle, style, False, "ERROR: INGEN MATCHANDE LUNCHTID")
+        write_text_turtle(window, turtle, style, False, "INGEN MATCHANDE LUNCHTID")
         # start_sound()
         return
     
@@ -125,7 +125,7 @@ def handle_enter(window, style):
     if(valid_lunch_time(times_match)):
         if hashed in used_tags:
             print("Dubbel skann")
-            write_text_turtle(window, turtle, style, False, "ERROR: DU HAR REDAN SKANNAT!")  
+            write_text_turtle(window, turtle, style, False, "DU HAR REDAN SKANNAT!")  
             return
 
         print("Godkänt")
@@ -189,7 +189,7 @@ if __name__ == '__main__':
     window.bgcolor("black")
     turtle.color('white')
     style = ('Roboto', 50, 'bold')
-    turtle.write("VÄNLIGEN SKANNA DIN NYCKELTAGG NEDAN", font=style, align='center')
+    turtle.write("VÄNLIGEN SKANNA DIN NYCKELTAGG TILL VÄNSTER", font=style, align='center')
 
     timer = None
 
