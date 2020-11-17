b1cace9c5a35cdc2d914008d4182cf8e4d588817
Pluppen <eliass.juremalm@gmail.com>
2020-11-17 10:40:54 +0100

    Add test case for nti and procivitas

--- test_saves_data.py
+++ test_saves_data.py
@@ -4,6 +4,7 @@ from pynput.keyboard import Key, Controller
 
 valid_tags = ["100331417", "101129785"]
 nti_tag = "100331417"
+procivitas_tag = "123456789"
 filename = "lunch_data.csv"
 
 keyboard = Controller()
@@ -72,6 +73,45 @@ try:
         line = f.readline()
         date, nti, procivitas = line.split(",")
         if nti.rstrip() == "2" and procivitas.rstrip() == "0":
+            print("TEST COMPLETE")
+        else:
+            print("TEST FAILED")
+except Exception as err:
+    print("TEST FAILED")
+    print(err)
+
+keyboard.press(Key.esc)
+keyboard.release(Key.esc)
+lunchpad.terminate()
+
+print("[*] Testing with 1 nti tag and 1 procivitas tag")
+try:
+    os.remove(filename)
+except:
+    pass
+lunchpad = subprocess.Popen(["python3", "lunchpad.py"])
+
+time.sleep(1)
+
+keyboard.type(nti_tag)
+keyboard.press(Key.enter)
+keyboard.release(Key.enter)
+
+time.sleep(1)
+
+keyboard.type(procivitas_tag)
+keyboard.press(Key.enter)
+keyboard.release(Key.enter)
+
+time.sleep(1)
+
+# Kolla efter csv filen
+try:
+    with open(filename, "r") as f:
+        f.readline() # Reads first line and does nothing with it.
+        line = f.readline()
+        date, nti, procivitas = line.split(",")
+        if nti.rstrip() == "1" and procivitas.rstrip() == "1":
             print("TEST COMPLETE")
         else:
             print("TEST FAILED")
