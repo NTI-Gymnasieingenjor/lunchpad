41921e6e587e8beafab70cfff603e4376d3a9cfd
Albin Nyman <joshei@telia.com>
2020-11-17 14:50:18 +0100

    Created test for uploading data

--- test_upload_data.py
+++ test_upload_data.py
@@ -1,26 +1,32 @@
-import gspread, csv, os
+import subprocess, gspread, os
 
-gc = gspread.service_account()
-sh = gc.open_by_key("11V4KfT00lrys2zHgLtRlF13q3SP-6n1CS_vbCyLmtqA")
-temp_filename = "temp_csv.csv"
-
-with open(temp_filename, 'w', newline='') as f:
-    writer = csv.writer(f)
+def check_data():
+    global test_filename
+    gc = gspread.service_account()
+    sh = gc.open_by_key("11V4KfT00lrys2zHgLtRlF13q3SP-6n1CS_vbCyLmtqA")
     worksheet = sh.worksheet("Lunchsystem")
-    writer.writerows(worksheet.get_all_values())
+    worksheet_value = worksheet.get_all_values()
+    test_content = open(test_filename, "r").read()
+    expected_result = list(map(lambda x: x.split(","), test_content.split("\n")))
 
-test_content = open("upload_data_test.csv", "r").read()
-gc.import_csv(sh.id, test_content.encode("utf8"))
+    if expected_result in worksheet_value:
+        print("\u001b[32mTest successful\u001b[0m")
+    else:
+        print("\u001b[31mTest failed\u001b[0m")
 
-worksheet_value = worksheet.get_all_values()
-expected_result = list(map(lambda x: x.split(","), test_content.split("\n")))
+    old_content = open("temp_old_data.csv", "r").read()
+    gc.import_csv(sh.id, old_content.encode("utf8"))
+    os.remove("temp_old_data.csv")
 
-if worksheet_value == expected_result:
-    print("\u001b[32mTest successful\u001b[0m")
+test_filename = "upload_data_test.csv"
+
+args = ["python3", "upload_data.py", "--csv", test_filename, "--test"]
+p = subprocess.Popen(args, stdout=subprocess.PIPE, stdin=subprocess.PIPE, shell=True)
+
+res = p.stdout.readline().decode("latin-1").strip()
+expected_res = "Upload complete."
+
+if res == expected_res:
+    check_data()
 else:
     print("\u001b[31mTest failed\u001b[0m")
-
-real_content = open(temp_filename, "r").read().encode("utf8")
-gc.import_csv(sh.id, real_content)
-
-os.remove(temp_filename)
