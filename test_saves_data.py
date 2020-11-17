c26476935c911d80053f2839f3eb99a4ff39468e
Pluppen <41504374+Pluppen@users.noreply.github.com>
2020-11-17 10:45:34 +0100

    Fix tests saves data (#12)
    * Fix tests
    
    * Add test case for nti and procivitas

--- test_saves_data.py
+++ test_saves_data.py
@@ -4,12 +4,17 @@ from pynput.keyboard import Key, Controller
 
 valid_tags = ["100331417", "101129785"]
 nti_tag = "100331417"
-procivitas_tag = "234567810"
+procivitas_tag = "123456789"
+filename = "lunch_data.csv"
 
 keyboard = Controller()
 
 # TEST FÖR GRÖNTAGG
 print("[*] Testing with 1 green tag")
+try:
+    os.remove(filename)
+except:
+    pass
 lunchpad = subprocess.Popen(["python3", "lunchpad.py"])
 
 time.sleep(1)
@@ -21,7 +26,6 @@ keyboard.release(Key.enter)
 time.sleep(1)
 
 # Kolla efter csv filen
-filename = "lunch_data.csv"
 try:
     with open(filename, "r") as f:
         # Kolla så csv filen har värdet 1 i rätt kolumn
@@ -42,6 +46,10 @@ lunchpad.terminate()
 
 # TEST FÖR TVÅ GRÖNTAGGAR
 print("[*] Testing with 2 green tags")
+try:
+    os.remove(filename)
+except:
+    pass
 lunchpad = subprocess.Popen(["python3", "lunchpad.py"])
 
 time.sleep(1)
@@ -59,7 +67,6 @@ keyboard.release(Key.enter)
 time.sleep(1)
 
 # Kolla efter csv filen
-filename = "lunch_data.csv"
 try:
     with open(filename, "r") as f:
         f.readline() # Reads first line and does nothing with it.
@@ -77,13 +84,15 @@ keyboard.press(Key.esc)
 keyboard.release(Key.esc)
 lunchpad.terminate()
 
-# TEST FÖR PROCIVITAS OCH NTI
 print("[*] Testing with 1 nti tag and 1 procivitas tag")
+try:
+    os.remove(filename)
+except:
+    pass
 lunchpad = subprocess.Popen(["python3", "lunchpad.py"])
 
 time.sleep(1)
 
-keyboard = Controller()
 keyboard.type(nti_tag)
 keyboard.press(Key.enter)
 keyboard.release(Key.enter)
@@ -97,46 +106,12 @@ keyboard.release(Key.enter)
 time.sleep(1)
 
 # Kolla efter csv filen
-filename = "lunch_data.csv"
 try:
     with open(filename, "r") as f:
         f.readline() # Reads first line and does nothing with it.
         line = f.readline()
         date, nti, procivitas = line.split(",")
         if nti.rstrip() == "1" and procivitas.rstrip() == "1":
-            print("TEST COMPLETE")
-        else:
-            print("TEST FAILED")
-except Exception as err:
-    print("TEST FAILED")
-    print(err)
-
-keyboard.press(Key.esc)
-keyboard.release(Key.esc)
-lunchpad.terminate()
-
-# TEST FÖR OLIKA DATUM
-print("[*] Testing with two different dates")
-date_to_test = "2020-11-16"
-lunchpad = subprocess.Popen(["python3", "lunchpad.py", "--date", date_to_test])
-
-time.sleep(1)
-
-keyboard = Controller()
-keyboard.type(nti_tag)
-keyboard.press(Key.enter)
-keyboard.release(Key.enter)
-
-time.sleep(1)
-
-# Kolla efter csv filen
-filename = "lunch_data.csv"
-try:
-    with open(filename, "r") as f:
-        f.readline() # Reads first line and does nothing with it.
-        line = f.readline()
-        date, nti, procivitas = line.split(",")
-        if date == date_to_test:
             print("TEST COMPLETE")
         else:
             print("TEST FAILED")
