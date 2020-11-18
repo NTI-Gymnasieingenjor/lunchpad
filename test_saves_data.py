47d3033bd4b490c3a230a821452c189ac1b4d1dd
Pluppen <41504374+Pluppen@users.noreply.github.com>
2020-11-18 11:40:25 +0100

    Add new test case to saves data test (#21)

--- test_saves_data.py
+++ test_saves_data.py
@@ -28,6 +28,19 @@ def test_students_eaten_saved(tags_to_blipp, nti_eaten, procivitas_eaten, date):
     # Resets the lunch_data.csv
     os.remove(filename)
 
+def test_students_eaten_append(tag, nti_eaten, procivitas_eaten, dates, expected_data):
+    failed = False
+    for date in dates:
+        handle_input(tag, tagsfile, timesfile, date, [], filename)
+
+    actual_data = None
+    with open(filename, "r") as f:
+        actual_data = f.readlines()
+
+    if actual_data != expected_data:
+        print("\u001b[32mTest successful\u001b[0m")
+    else:
+        print("\u001b[31mTest failed\u001b[0m")
 
 if __name__ == "__main__":
     valid_tags = ["***REMOVED***", "***REMOVED***"]
@@ -51,3 +64,8 @@ if __name__ == "__main__":
 
     print("[*] Testing with 1 green tag from NTI on date 2020-12-24")
     test_students_eaten_saved([nti_tag], "1", "0", datetime.datetime(2020, 12, 24, 12, 10, 10))
+
+    print("[*] Testing with 1 green tag one date then 1 green tag another date")
+    expected_data = ["DATUM,NTI,PROCIVITAS\n", "2020-11-10,1,0\n", "2020-11-11,1,0\n"]
+    dates = [datetime.datetime(2020, 11, 10, 12, 10, 10), datetime.datetime(2020, 11, 11, 12, 10, 10)]
+    test_students_eaten_append(nti_tag, "1", "0", dates, expected_data)
