25c9d78ff53d955655ff2efe324c1169a7143d9f
Emil Andersen <54682521+piloteer44@users.noreply.github.com>
2020-11-19 11:03:04 +0100

    Added test cases for weekends, started using unittest framework and added os check to run test (#26)
    * Added unittest framework to test_blipp_cases test file
    
    * REMOVED PYCHACHE

--- test_blipp_cases.py
+++ test_blipp_cases.py
@@ -1,86 +1,96 @@
 #!/usr/bin/env python3
-import subprocess, time, psutil, sys
+import subprocess, time
 from pynput.keyboard import Key, Controller
 from lunchpad import *
 import datetime
+import platform
+import unittest
 
-def correct_output():
-    global tests
-    time.sleep(2)
-    keyboard = Controller()
+class TestLunchpad(unittest.TestCase):
 
-    for test in tests:
-        keyboard.type(test[0])
-        keyboard.press(Key.enter)
-        keyboard.release(Key.enter)
-        time.sleep(1)
+    def test_correct_output(self):
+        global tests
+        time.sleep(2)
+        keyboard = Controller()
+
+        for test in tests:
+            keyboard.type(test[0])
+            keyboard.press(Key.enter)
+            keyboard.release(Key.enter)
+            time.sleep(1)
+            
+        keyboard.press(Key.esc)
+        keyboard.release(Key.esc)
+
+        for test in tests:
+            res = p.stdout.readline().decode("latin-1").strip()
+            self.assertEqual(test[1], res)
+
+    def test_correct_text(self):
+
+        # Scanned tag is on time
+        actual = handle_input("101129785", tags, times, datetime.datetime(2020, 11, 11, 12, 10, 10),[],"test_data.csv")
+        expected = True, "GODKÄND SKANNING! SMAKLIG MÅLTID!"
+        self.assertEqual(expected, actual)
+
+        # Scanned tag is first in list
+        actual = handle_input("101129785", tags, times, datetime.datetime(2020, 11, 11, 12, 10, 10),["c3f4286f131e08c4b8d1570bee6a4a44250834d3c44aecae7c10ed9e1e7a98c5", "2711ec7bebeff204b1d6d39cc8dcbfbef44d93da919befb9834a839d98a5e1bf"],"test_data.csv")
+        expected = False, "DU HAR REDAN SKANNAT"
+        self.assertEqual(expected, actual)
+
+        # Scanned tag is last in list
+        actual = handle_input("101129785", tags, times, datetime.datetime(2020, 11, 11, 12, 10, 10),["2711ec7bebeff204b1d6d39cc8dcbfbef44d93da919befb9834a839d98a5e1bf", "c3f4286f131e08c4b8d1570bee6a4a44250834d3c44aecae7c10ed9e1e7a98c5"],"test_data.csv")
+        expected = False, "DU HAR REDAN SKANNAT"
+        self.assertEqual(expected, actual)
+
+        # Scanned tag is in the middle of the list
+        actual = handle_input("101129785", tags, times, datetime.datetime(2020, 11, 11, 12, 10, 10),["2711ec7bebeff204b1d6d39cc8dcbfbef44d93da919befb9834a839d98a5e1bf", "c3f4286f131e08c4b8d1570bee6a4a44250834d3c44aecae7c10ed9e1e7a98c5", "1514aa8277131f9dc6c69f615fae6e15cce2022237a5c98a3533f2007bb99aba"],"test_data.csv")
+        expected = False, "DU HAR REDAN SKANNAT"
+        self.assertEqual(expected, actual)
+
+        # Scanned tag is off time
+        actual = handle_input("934219478", tags, times, datetime.datetime(2020, 11, 11, 12, 10, 10),[],"test_data.csv")
+        expected = False, "DIN LUNCHTID ÄR 11:00-11:20"
+        self.assertEqual(expected, actual)
         
-    keyboard.press(Key.esc)
-    keyboard.release(Key.esc)
+        actual = handle_input("101051865", tags, times, datetime.datetime(2020, 11, 11, 12, 10, 10),[],"test_data.csv")
+        expected = False, "INGEN MATCHANDE LUNCHTID"
+        self.assertEqual(expected, actual)
 
-    for test in tests:
-        res = p.stdout.readline().decode("latin-1").strip()
-        check_test(test[1], res)
+        actual = handle_input("123456788", tags, times, datetime.datetime(2020, 11, 11, 12, 10, 10),[],"test_data.csv")
+        expected = False, "OKÄND NYCKELTAGG"
+        self.assertEqual(expected, actual)
 
-def correct_text():
-
-    actual = handle_input("101129785", tags, times, datetime.datetime(2020, 11, 11, 12, 10, 10),[])
-    expected = True, "GODKÄND SKANNING! SMAKLIG MÅLTID!"
-    check_test(expected, actual)
-
-    actual = handle_input("934219478", tags, times, datetime.datetime(2020, 11, 11, 12, 10, 10),[])
-    expected = False, "DIN LUNCHTID ÄR 11:00-11:20"
-    check_test(expected, actual)
-    
-    actual = handle_input("101051865", tags, times, datetime.datetime(2020, 11, 11, 12, 10, 10),[])
-    expected = False, "INGEN MATCHANDE LUNCHTID"
-    check_test(expected, actual)
-
-    actual = handle_input("123456789", tags, times, datetime.datetime(2020, 11, 11, 12, 10, 10),[])
-    expected = False, "OKÄND NYCKELTAGG"
-    check_test(expected, actual)
-
-    actual = handle_input("934219478", tags, times, datetime.datetime(2020, 11, 14, 12, 10, 10),[])
-    expected = False, "DIN LUNCHTID ÄR 00:00-00:00"
-    check_test(expected, actual)
+        actual = handle_input("934219478", tags, times, datetime.datetime(2020, 11, 14, 12, 10, 10),[],"test_data.csv")
+        expected = False, "DIN LUNCHTID ÄR 00:00-00:00"
+        self.assertEqual(expected, actual)
 
 
-def time_test():
+    def test_correct_time(self):
 
-    # On time for lunch
-    correct_time = valid_lunch_time(["TE4","12:10-12:30","12:10-12:30","12:10-12:30","12:30-12:50","12:30-12:50"], datetime.datetime(2020, 11, 11, 12, 10, 10))
-    # 1 minute before lunch time
-    wrong_before_1min = valid_lunch_time(["TE4","12:10-12:30","12:10-12:30","12:10-12:30","12:30-12:50","12:30-12:50"], datetime.datetime(2020, 11, 11, 12, 9, 10))
-    # 1 minute after lunch time
-    wrong_after_1min = valid_lunch_time(["TE4","12:10-12:30","12:10-12:30","12:10-12:30","12:30-12:50","12:30-12:50"], datetime.datetime(2020, 11, 11, 12, 9, 10))
-    # Midnight
-    wrong_time_midnight = valid_lunch_time(["TE4","12:10-12:30","12:10-12:30","12:10-12:30","12:30-12:50","12:30-12:50"], datetime.datetime(2020, 11, 11, 0, 0, 1))
-    # Weekend
-    wrong_time_weekend = valid_lunch_time(["TE4","12:10-12:30","12:10-12:30","12:10-12:30","12:30-12:50","12:30-12:50"], datetime.datetime(2020, 11, 14, 16, 0, 1))
+        # On time for lunch
+        correct_time = valid_lunch_time(["TE4","12:10-12:30","12:10-12:30","12:10-12:30","12:30-12:50","12:30-12:50"], datetime.datetime(2020, 11, 11, 12, 10, 10))
+        # 1 minute before lunch time
+        wrong_before_1min = valid_lunch_time(["TE4","12:10-12:30","12:10-12:30","12:10-12:30","12:30-12:50","12:30-12:50"], datetime.datetime(2020, 11, 11, 12, 9, 10))
+        # 1 minute after lunch time
+        wrong_after_1min = valid_lunch_time(["TE4","12:10-12:30","12:10-12:30","12:10-12:30","12:30-12:50","12:30-12:50"], datetime.datetime(2020, 11, 11, 12, 9, 10))
+        # Midnight
+        wrong_time_midnight = valid_lunch_time(["TE4","12:10-12:30","12:10-12:30","12:10-12:30","12:30-12:50","12:30-12:50"], datetime.datetime(2020, 11, 11, 0, 0, 1))
+        # Weekend
+        wrong_time_weekend = valid_lunch_time(["TE4","12:10-12:30","12:10-12:30","12:10-12:30","12:30-12:50","12:30-12:50"], datetime.datetime(2020, 11, 14, 16, 0, 1))
 
-    check_test(True, correct_time)
-    check_test(False, wrong_before_1min)
-    check_test(False, wrong_after_1min)
-    check_test(False, wrong_time_midnight)
-    check_test(False, wrong_time_weekend)
-    
-def check_test(expected, actual):
-    if expected == actual:
-        print("\u001b[32mTest successful\u001b[0m")
-    else:
-        print("\u001b[31mTest failed\u001b[0m")
-        print("Expected:", expected)
-        print("Actual:", actual)
-        fail + 1
-        return fail
-
-
-def fail_check(fail):
-    if fail >= 1:
-        sys.exit(1)
+        self.assertEqual(True, correct_time)
+        self.assertEqual(False, wrong_before_1min)
+        self.assertEqual(False, wrong_after_1min)
+        self.assertEqual(False, wrong_time_midnight)
+        self.assertEqual(False, wrong_time_weekend)
 
 if __name__ == '__main__':
-    args = ["python3","lunchpad.py","-test"]
+
+    if platform.system() == "Linux":
+        args = ["python3","lunchpad.py","-test"]
+    else:
+        args = ["python","lunchpad.py","-test"]
 
     file = os.path.dirname(os.path.realpath(__file__))
 
@@ -89,19 +99,10 @@ if __name__ == '__main__':
     tags = get_file_data(file+"/id_tester.csv", "tags")
     times = get_file_data(file+"/tider_tester.csv", "times")
 
-
     tests = [
         ["12348910", "OKÄND NYCKELTAGG"],
         ["101129785", "GODKÄND SKANNING! SMAKLIG MÅLTID!"],
         ["101129785", "DU HAR REDAN SKANNAT"]
     ]
 
-    fail = 0
-
-    print("Correct_output")
-    correct_output()
-    print("Time_test")
-    time_test()
-    print("Correct_text")
-    correct_text()
-    fail_check(fail)
+    unittest.main()
