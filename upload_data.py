7252d2201243a6a0634d77cb48e0afbf47f137d7
Pluppen <41504374+Pluppen@users.noreply.github.com>
2020-11-23 13:54:22 +0100

    Fix timeout bug (#40)
    * Added Timeout error excepts
    
    * Fix to check for correct exception if no network
    
    Co-authored-by: DanTGL <33489389+DanTGL@users.noreply.github.com>

--- upload_data.py
+++ upload_data.py
@@ -1,6 +1,7 @@
 import gspread
 import sys
 import os
+from google.auth.exceptions import *
 from datetime import datetime
 
 
@@ -20,58 +21,75 @@ def upload_data(data):
             worksheet.update_cell(idx + 1, "2", nti)
             worksheet.update_cell(idx + 1, "3", procivitas)
     except Exception as err:
-        print(err)
+        if type(err) == TransportError:
+            print("\u001b[31mTimed out:\n   Retry. Try connecting to another network if not working.\u001b[0m")
+        else:
+            print(err)
         sys.exit(1)
 
 
 if __name__ == '__main__':
+    try:
+        gc = gspread.service_account()
+        sh = gc.open_by_key("11V4KfT00lrys2zHgLtRlF13q3SP-6n1CS_vbCyLmtqA")
+        worksheet = sh.worksheet("Lunchsystem")
 
-    gc = gspread.service_account()
-    sh = gc.open_by_key("11V4KfT00lrys2zHgLtRlF13q3SP-6n1CS_vbCyLmtqA")
-    worksheet = sh.worksheet("Lunchsystem")
+        data_file = "lunch_data.csv"
 
-    data_file = "lunch_data.csv"
+        if "--csv" in sys.argv:
+            try:
+                data_file = sys.argv[sys.argv.index("--csv") + 1] 
+            except IndexError as err:
+                print(err)
+                print("\u001b[31mNo file was specified\u001b[0m.")
+                sys.exit(1)
+      
+      if not os.path.isfile(data_file):
+          print("\u001b[31mCould not read file: {}\u001b[0m".format(data_file))
+          sys.exit(1)
 
-    if "--csv" in sys.argv:
+          if "--test" in sys.argv:
+              worksheet = sh.worksheet("TEST")
+
+          local_data = None
+          sheet_data = list(map(lambda x: ",".join(x), worksheet.get_all_values()))
+          formatted_sheet_data = []
+          combined_data = []
+          for idx, row in enumerate(sheet_data):
+              if idx != 0:
+                  new_string = row.split(" ")[0]
+                  new_string += "," + row.split(",")[1]
+                  new_string += "," + row.split(",")[2]
+                  formatted_sheet_data.append(new_string)
+              else:
+                  formatted_sheet_data.append(row)
+
+      with open(data_file, 'r') as f:
+          local_data = f.read().split("\n")
+
+      try:
+          with open(data_file, 'r') as f:
+              local_data = f.read().split("\n")
+      except Exception:
+          print("\u001b[31mCould not read file: {}\u001b[0m".format(data_file))
+          sys.exit(1)
+
         try:
-            data_file = sys.argv[sys.argv.index("--csv") + 1]
-        except IndexError as err:
-            print(err)
-            print("\u001b[31mNo file was specified\u001b[0m.")
+            with open(data_file, 'r') as f:
+                local_data = f.read().split("\n")
+        except:
+            print("\u001b[31mCould not read file: {}\u001b[0m".format(data_file))
             sys.exit(1)
 
-    if not os.path.isfile(data_file):
-        print("\u001b[31mCould not read file: {}\u001b[0m".format(data_file))
-        sys.exit(1)
+        local_data = list(filter(lambda elem: elem != "", local_data))
+        combined_data = local_data + formatted_sheet_data
+        unique_data = list(set(combined_data))
 
-    if "--test" in sys.argv:
-        worksheet = sh.worksheet("TEST")
+        upload_data(unique_data)
 
-    local_data = None
-    sheet_data = list(map(lambda x: ",".join(x), worksheet.get_all_values()))
-    formatted_sheet_data = []
-    combined_data = []
-    for idx, row in enumerate(sheet_data):
-        if idx != 0:
-            new_string = row.split(" ")[0]
-            new_string += "," + row.split(",")[1]
-            new_string += "," + row.split(",")[2]
-            formatted_sheet_data.append(new_string)
+    except Exception as err:
+        if type(err) == TransportError:
+            print("\u001b[31mTimed out:\n   Retry. Try connecting to another network if not working.\u001b[0m")
         else:
-            formatted_sheet_data.append(row)
-
-    with open(data_file, 'r') as f:
-        local_data = f.read().split("\n")
-
-    try:
-        with open(data_file, 'r') as f:
-            local_data = f.read().split("\n")
-    except Exception:
-        print("\u001b[31mCould not read file: {}\u001b[0m".format(data_file))
+            print(err)
         sys.exit(1)
-
-    local_data = list(filter(lambda elem: elem != "", local_data))
-    combined_data = local_data + formatted_sheet_data
-    unique_data = list(set(combined_data))
-
-    upload_data(unique_data)
