efdcb2aea79df2c478c59a56b99b8b522f0d68c0
DanTGL <33489389+DanTGL@users.noreply.github.com>
2020-11-23 11:45:46 +0100

    Added Timeout error excepts

--- upload_data.py
+++ upload_data.py
@@ -16,60 +16,67 @@ def upload_data(data):
             worksheet.update_cell(idx + 1, "1", date)
             worksheet.update_cell(idx + 1, "2", nti)
             worksheet.update_cell(idx + 1, "3", procivitas)
+    except TimeoutError:
+        print("\u001b[31mTimed out:\n   Retry. Try connecting to another network if not working.\u001b[0m")
+        sys.exit(1)
     except Exception as err:
         print(err)
         sys.exit(1)
 
 if __name__ == '__main__':
-
-    gc = gspread.service_account()
-    sh = gc.open_by_key("11V4KfT00lrys2zHgLtRlF13q3SP-6n1CS_vbCyLmtqA")
-    worksheet = sh.worksheet("Lunchsystem")
-
-    data_file = "lunch_data.csv"
-
-    if "--csv" in sys.argv:
-        try:
-            data_file = sys.argv[sys.argv.index("--csv") + 1] 
-        except IndexError as err:
-            print(err)
-            print("\u001b[31mNo file was specified\u001b[0m.")
-            sys.exit(1)
-    
-    if not os.path.isfile(data_file):
-        print("\u001b[31mCould not read file: {}\u001b[0m".format(data_file))
-        sys.exit(1)
-
-    if "--test" in sys.argv:
-        worksheet = sh.worksheet("TEST")
-
-    local_data = None
-    sheet_data = list(map(lambda x: ",".join(x), worksheet.get_all_values()))
-    formatted_sheet_data = []
-    combined_data = []
-    for idx, row in enumerate(sheet_data):
-        if idx != 0:
-            new_string = row.split(" ")[0]
-            new_string += "," + row.split(",")[1]
-            new_string += "," + row.split(",")[2]
-            formatted_sheet_data.append(new_string)
-        else:
-            formatted_sheet_data.append(row)
-
-    with open(data_file, 'r') as f:
-        local_data = f.read().split("\n")
-
-    
     try:
+        gc = gspread.service_account()
+        sh = gc.open_by_key("11V4KfT00lrys2zHgLtRlF13q3SP-6n1CS_vbCyLmtqA")
+        worksheet = sh.worksheet("Lunchsystem")
+
+        data_file = "lunch_data.csv"
+
+        if "--csv" in sys.argv:
+            try:
+                data_file = sys.argv[sys.argv.index("--csv") + 1] 
+            except IndexError as err:
+                print(err)
+                print("\u001b[31mNo file was specified\u001b[0m.")
+                sys.exit(1)
+
+        if not os.path.isfile(data_file):
+            print("\u001b[31mCould not read file: {}\u001b[0m".format(data_file))
+            sys.exit(1)
+
+        if "--test" in sys.argv:
+            worksheet = sh.worksheet("TEST")
+
+        local_data = None
+        sheet_data = list(map(lambda x: ",".join(x), worksheet.get_all_values()))
+        formatted_sheet_data = []
+        combined_data = []
+        for idx, row in enumerate(sheet_data):
+            if idx != 0:
+                new_string = row.split(" ")[0]
+                new_string += "," + row.split(",")[1]
+                new_string += "," + row.split(",")[2]
+                formatted_sheet_data.append(new_string)
+            else:
+                formatted_sheet_data.append(row)
+
         with open(data_file, 'r') as f:
             local_data = f.read().split("\n")
-    except:
-        print("\u001b[31mCould not read file: {}\u001b[0m".format(data_file))
+
+
+        try:
+            with open(data_file, 'r') as f:
+                local_data = f.read().split("\n")
+        except:
+            print("\u001b[31mCould not read file: {}\u001b[0m".format(data_file))
+            sys.exit(1)
+
+        local_data = list(filter(lambda elem: elem != "", local_data))
+        combined_data = local_data + formatted_sheet_data
+        unique_data = list(set(combined_data))
+
+
+        upload_data(unique_data)
+
+    except TimeoutError:
+        print("\u001b[31mTimed out:\n   Retry. Try connecting to another network if not working.\u001b[0m")
         sys.exit(1)
-
-    local_data = list(filter(lambda elem: elem != "", local_data))
-    combined_data = local_data + formatted_sheet_data
-    unique_data = list(set(combined_data))
-
-
-    upload_data(unique_data)
